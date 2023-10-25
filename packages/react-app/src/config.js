38de0a22c3559ea14130f7805ee193d7f17c2007
ibrainiac <franqhazard@gmail.com>
2023-10-26 01:00:07 +0300

    creating hooks and front end of  the DEX

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,10 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = "0x71b3Aa00064c87d0FeEFE6C2aD91b1F01d0E389D"; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/UymYcwXW_fIxUG-7ksqCfwUKteFwB6iF",
   },
 };
